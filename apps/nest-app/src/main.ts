518a9959b7bc997a39564a4ec5f27f687121207a
Hichchi Dev <hichchidev@gmail.com>
2025-06-23 02:11:49 +0530

    test publish

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -4,4 +4,5 @@ import { AppModule } from "./app.module";
 import configuration from "./core/config/configuration";
 import { hichchiBootstrap } from "@test-group/nest-core";
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 hichchiBootstrap(AppModule, { allowedOrigins: configuration().app.allowedOrigins });
